c72bd1269919416240abb516d97f75075db2ad8d
khoa23032003 <2minhkhoa23032003@gmail.com>
2026-06-01 15:26:31 +0700

    create About page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/Home/About/About";
 import Hero from "@/components/Home/Hero/Hero";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <About />
     </div>
   );
 };
